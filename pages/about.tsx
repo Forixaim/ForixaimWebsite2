815516f12a06b32d7063530533c3e7879c195fec
Matthew <thebest20067@gmail.com>
2022-05-18 08:05:54 -0700

    fixes

--- pages/about.tsx
+++ pages/about.tsx
@@ -16,8 +16,8 @@ const About: NextPage = () =>
 				<Navbar/>
 		</header>
 		<main className={styles.main}>
-				<h1 className={styles.title}>About Forixaim</h1>
-				<p className={styles.description}>Hey! I am Matthew, a C++ programmer, I am the CTO of 2022-2023 Newport High School Programming Club. Fresh out of the pandemic, I was thrown into some hard times, but perserverd and broke free.</p>
+			<h1 className={styles.title}>About Forixaim</h1>
+			<p className={styles.description}>Hey! I am Matthew, a C++ programmer, I am the CTO of 2022-2023 Newport High School Programming Club. Fresh out of the pandemic, I was thrown into some hard times, but perserverd and broke free.</p>
 		</main>
 		<footer>
 			<p>Website by Forixaim. Powered by Next.js!</p>
