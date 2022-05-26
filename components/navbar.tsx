85cb8ad44e2bfc3a1500f956f16c3de1dcdaa9b3
Matthew <thebest20067@gmail.com>
2022-05-25 21:54:20 -0700

    true

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,13 +3,13 @@ import React from "react"
 import styles from '../styles/NavBar.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
-import logo from '../public/charlesmall.png'
+import ForiIcon from '../public/Forixaim-Icon.svg'
 
 const Navbar = () => {
 	return (
 		<nav className={styles.NavBarColor}>
 			<div>
-				<Link href="/"><a className={styles.NavIcon}><Image src={logo} width={50} height={50}/></a></Link>
+				<Link href="/"><a className={styles.NavIcon}><Image src={ForiIcon} width={50} height={50}/></a></Link>
 				<Link href="projects"><a className={styles.NavLink}>Projects</a></Link>
 				<Link href="contact"><a className={styles.NavLink}>Contact</a></Link>
 				<Link href="donate"><a className={styles.NavLink}>Donate</a></Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/navbar'
 import "bootstrap/dist/css/bootstrap.min.css"
-import charlemagne from '../public/charlemagne.png'
+import Forixaim from '../public/Forixaim.svg'
 
 const Home: NextPage = () => {
 	return (
@@ -18,8 +18,15 @@ const Home: NextPage = () => {
 				<Navbar/>
 			</header>
 			<main className={styles.main}>
-				<h1 className={styles.title}>Forixaim</h1>
-				<Image alt="Charlemagne" src={charlemagne} width={235} height={365} className={styles.runningCharle} />
+				<div>
+					<div style={{
+          display: "flex",
+          justifyContent: "center",
+        }}>
+						<Image src={Forixaim} width={1000}/>
+					</div>
+				</div>
+				
 				<p className={styles.description}>Hello there I am Forixaim, the C++ guy. This new website will replace my old website. I don&apos;t know how to use TypeScript, so help would be awesome! Alright, use the navigation bar above to navigate to other aspects of the page, there we can have some fun.</p>
 			</main>
 			<footer>
