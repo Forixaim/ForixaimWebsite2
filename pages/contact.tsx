1d2efb8187f1fafcb679be883691cbe02c0ea2fe
Matthew <thebest20067@gmail.com>
2022-05-25 22:59:18 -0700

    funky patreon

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -16,11 +16,13 @@ const Contact: NextPage = () =>
 				<Navbar />
 			</header>
 			<main className={styles.main}>
-				<div>
+			<	div className={styles.discord}>
+					<iframe src="https://discord.com/widget?id=941463674361622578&theme=dark" width="350" height="500"  frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" className={styles.discord}/>
+				</div>
+				<div className={styles.content}>
 					<h1 className={styles.title}>Contact Me</h1>
 					<p className={styles.description}>You may contact me at Forixaim#5359 at Discord or email me at matthewcs@forixaim.net, just be aware, spam emails or spam messages will be ignored.</p>
 				</div>
-				<iframe src="https://discord.com/widget?id=941463674361622578&theme=dark" width="350" height="500"  frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" className={styles.discord}/>
 			</main>
 			<footer>
 				<p>Website by Forixaim. Powered by Next.js!</p>
