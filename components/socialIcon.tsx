45c41944297cb79373fb130d43994cfb12c071a5
Matthew <thebest20067@gmail.com>
2022-07-26 20:43:29 -0400

    fixed security risk

--- components/socialIcon.tsx
+++ components/socialIcon.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const SocialIcon = (props: any) => {
     return (
         <div className="rounded-2xl bg-gradient-to-tr via-slate-600 from-cyan-900 to-cyan-900 p-2 m-4 w-16 h-16 cursor-pointer">
-            <a href={props.url} target="_blank"><Image src={props.imgurl} width={100} height={100} alt={props.altText}/></a>
+            <a href={props.url}  target="_blank" rel="noreferrer"><Image src={props.imgurl} width={100} height={100} alt={props.altText}/></a>
         </div>
     )
 }
